d8e49cafa875e0ea4df013b3f32533b7a9ea88b4
Colm O'Connor <colm.oconnor.github@gmail.com>
2014-08-30 18:26:36 +0800

    Bumped version

--- devkey/__init__.py
+++ devkey/__init__.py
@@ -4,7 +4,7 @@
 import interpreter, sys
 
 # The version as used in the setup.py and the docs conf.py
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 if __name__ == '__main__':
     exit = interpreter.interpreter()
