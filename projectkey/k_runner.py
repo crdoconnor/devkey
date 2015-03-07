7897b985a1ec8fe240c9edead2b53567221cd095
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-03-07 20:01:57 +0800

    Convert print statement to sys.stderr.write()

--- projectkey/k_runner.py
+++ projectkey/k_runner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # PYTHON_ARGCOMPLETE_OK
-import os, imp, inspect
+import os, imp, inspect, sys
 from interpreter import cli_interface
 
 def k_runner():
@@ -18,9 +18,9 @@ def k_runner():
             checkdirectory = os.path.abspath(os.path.join(checkdirectory, os.pardir))
 
     if not keypy_filename:
-        print "key.py not found in the following directories:\n"
-        print '\n'.join(directories_checked)
-        print "\nSee http://projectkey.readthedocs.org/en/latest/quickstart.html"
-        return 1
+        sys.stderr.write("key.py not found in the following directories:\n\n")
+        sys.stderr.write('\n'.join(directories_checked))
+        sys.stderr.write("\n\nSee http://projectkey.readthedocs.org/en/latest/quickstart.html\n")
+        sys.exit(1)
     else:
         cli_interface(imp.load_source("key", keypy_filename))
